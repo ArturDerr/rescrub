2510b4ad03233270eb15671cba2aefa0db691df2
Artur Derr <artur12derr@gmail.com>
2025-08-17 00:49:12 +0300

    auth logic & error handler in auth.ts & add ui library ShadcnUI

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,59 @@
+import { API_URL } from "../constants/constants";
+import type { ILogin, ILoginResponse, IRegister, IRegisterResponse } from "../interfaces";
+
+// обработка ошибок 
+
+const errorsResponse = async <T>(response: Response): Promise<T> => {
+    let data: any
+
+    // сделать красивый вывод ошибки в alert позже
+    try {
+        data = await response.json()
+    } catch {
+        throw new Error("Некорректный ответ от сервера, попробуйте позже")
+    }
+
+    if (!response.ok) {
+        const errorMessage = data?.message || data?.detail || "Незвестная ошибка, попробуйте позже"
+        throw new Error(errorMessage)
+    }   
+
+    return data as T
+
+}
+
+// регистрация 
+
+export const registerUser = async (payload: IRegister): Promise<IRegisterResponse> => {
+    try {
+        const response = await fetch(`${API_URL}/register/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload)
+        })
+        
+        return await errorsResponse<IRegisterResponse>(response)
+    } catch (error: any) {
+        console.error("Ошибка регистрации", error)
+        throw error
+    }
+}
+
+// логин 
+
+export const loginUser = async (payload: ILogin): Promise<ILoginResponse> => {
+    try {
+        const response = await fetch(`${API_URL}/login/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload)
+        })
+        
+        return await errorsResponse<ILoginResponse>(response)
+    } catch (error: any) {
+        console.error("Ошибка входа", error)
+        throw error
+    }
+}
+
+
--- frontend/src/interfaces/index.ts
+++ frontend/src/interfaces/index.ts
@@ -2,14 +2,14 @@ import type { Transition, Variants } from "motion";
 import type { AnimatePresenceProps } from "motion/react";
 
 export type TextLoopProps = {
-  children: React.ReactNode[];
-  className?: string;
-  interval?: number;
-  transition?: Transition;
-  variants?: Variants;
-  onIndexChange?: (index: number) => void;
-  trigger?: boolean;
-  mode?: AnimatePresenceProps['mode'];
+  children: React.ReactNode[]
+  className?: string
+  interval?: number
+  transition?: Transition
+  variants?: Variants
+  onIndexChange?: (index: number) => void
+  trigger?: boolean
+  mode?: AnimatePresenceProps['mode']
 };
 
 export interface IDataStore {
@@ -18,11 +18,42 @@ export interface IDataStore {
 }
 
 export interface IInputProps {
-  type: string;
-  id: string;
-  name: string;
-  placeholder: string;
-  inputRef?: React.Ref<HTMLInputElement>;
+  type: string
+  id: string
+  name: string
+  placeholder: string
+  inputRef?: React.Ref<HTMLInputElement>
   value?: string
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
+}
+
+export interface IRegister {
+  name: string
+  surname: string
+  lastname?: string
+  email: string
+  birthDate: Date
+  password: string
+}
+
+export interface ILogin {
+  email: string
+  password: string
+}
+
+
+export interface IUser {
+  id: number
+  username: string
+  email: string
+}
+
+export interface IRegisterResponse {
+  user: IUser
+  token: string
+}
+
+export interface ILoginResponse {
+  access: string
+  refresh: string
 }
